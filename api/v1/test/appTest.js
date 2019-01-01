63821991bdca475db24199fb78e4936db0316f3b
wptechprodigy <waheed.freelancer16@gmail.com>
2019-01-01 17:37:55 +0100

    remove gitignore files

--- api/v1/test/appTest.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-const server = require('../server');
-
-describe('test', () => {
-  it('should return a string', () => {
-    expect('Great. Basic API setup completed!').to.equal('Great. Basic API setup completed!');
-  });
-});
-
