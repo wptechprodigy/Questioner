76b32e2b6d5aba6d5714763748a3da6e060f766c
wptechprodigy <waheed.freelancer16@gmail.com>
2019-01-05 14:07:31 +0100

    Added meetup routes

--- endpoints/v1/app.js
+++ endpoints/v1/app.js
@@ -1,7 +1,21 @@
-const sayHello = () => {
-  return 'Hello Andelans!';
-};
+import bodyParser from 'body-parser';
+import express from 'express';
 
-sayHello();
+import meetupRoutes from './api/routes/meetups';
 
-export default sayHello;
+// Set up the express app
+const app = express();
+
+// Parse incoming request data
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use('/api/v1/meetups', meetupRoutes);
+  
+// Set up listening port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
